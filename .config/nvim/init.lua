10d22014c80e62a4822592763073c3bfe4f9ec1e
nx-smul <shimulkhn10@gmail.com>
2024-10-24 21:32:34 +0600

    heda

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,43 +1,21 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
-vim.g.mapleader = " "
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
 
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+-- Initialize neopywal
+local neopywal = require("neopywal")
+neopywal.setup()
 
-local nvim_config_path = vim.fn.stdpath('config')
-local python_script = nvim_config_path .. "/pywal/chadwal.py"
-os.execute("python3 " .. python_script .. " &> /dev/null &")
+-- Apply the neopywal colorscheme
+vim.cmd.colorscheme("neopywal")
 
-if not vim.loop.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-local lazy_config = require "configs.lazy"
-
--- load plugins
-require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-    config = function()
-      require "options"
-    end,
+-- NeoTree enable hidden files
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+      hide_dotfiles = false,
+      hide_gitignored = true,
+    },
   },
-
-  { import = "plugins" },
-}, lazy_config)
-
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
-
-require "nvchad.autocmds"
-
-vim.schedule(function()
-  require "mappings"
-end)
+})
+-- unrelated
