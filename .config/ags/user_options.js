cf752980829da9750fda29a3ed00397422349bc8
nx-smul <shimulkhn10@gmail.com>
2024-11-06 15:31:10 +0600

    heda

--- .config/ags/user_options.js
+++ .config/ags/user_options.js
@@ -7,12 +7,12 @@ const userConfigOptions = {
   "appearance": {
     "barRoundCorners": 0, // 0: No, 1: Yes
     "fakeScreenRounding": 0, // 0: None | 1: Always | 2: When not fullscreen
-   },
+  },
   'workspaces': {
-        'shown': 5, 
-   },
- 'weather': {
-    'city': "Dhaka",
+    'shown': 5,
+  },
+  'weather': {
+    'city': "Faridpur",
     'preferredUnit': "C", // Either C or F
   },
 }
